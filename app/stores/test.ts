cc5fb353bf36584ab01934781e07297655542544
Lumirelle <shabbyacc@outlook.com>
2025-08-06 18:11:22 +0800

    feat: :sparkles: add stores demo

---
+++ app/stores/test.ts
@@ -0,0 +1,21 @@
+export const useTestStore = defineStore('test', {
+  state: () => ({
+    count: 0,
+  }),
+  getters: {
+    doubleCount: state => state.count * 2,
+    tripleCount: state => state.count * 3,
+    negativeCount: state => -state.count,
+  },
+  actions: {
+    increment() {
+      this.count++
+    },
+    decrement() {
+      this.count--
+    },
+    reset() {
+      this.count = 0
+    },
+  },
+})
