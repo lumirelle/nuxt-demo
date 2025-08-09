e822c3517121620f3b0eeeef4540d9602bace420
Lumirelle <shabbyacc@outlook.com>
2025-08-09 14:15:27 +0800

    fix: props name

--- app/components/HighlightJs.vue
+++ app/components/HighlightJs.vue
@@ -5,12 +5,23 @@
 import hljs from 'highlight.js/lib/core'
 
 import javascript from 'highlight.js/lib/languages/javascript'
+import typescript from 'highlight.js/lib/languages/typescript'
 import xml from 'highlight.js/lib/languages/xml'
+import yaml from 'highlight.js/lib/languages/yaml'
 import python from 'highlight.js/lib/languages/python'
 // Just import the language you need above
 
+hljs.registerLanguage('yaml', yaml)
+hljs.registerLanguage('xml', xml)
+hljs.registerLanguage('javascript', javascript)
+hljs.registerLanguage('typescript', typescript)
+hljs.registerLanguage('python', python)
+// Just register the language you need above
+
+// eslint-disable-next-line import/first
 import HighlightJSVue from '@highlightjs/vue-plugin'
 
+// eslint-disable-next-line vue/define-macros-order
 const props = defineProps({
   code: {
     type: String,
@@ -20,7 +31,7 @@ const props = defineProps({
     type: String,
     default: '',
   },
-  autoDetect: {
+  autodetect: {
     type: Boolean,
     default: true,
   },
@@ -30,18 +41,13 @@ const props = defineProps({
   },
 })
 
-hljs.registerLanguage('xml', xml)
-hljs.registerLanguage('javascript', javascript)
-hljs.registerLanguage('python', python)
-// Just register the language you need above
-
 function Render() {
   return h(
     HighlightJSVue.component,
     {
       language: props.language,
       code: props.code,
-      autoDetect: props.autoDetect,
+      autodetect: props.autodetect,
       ignoreIllegals: props.ignoreIllegals,
     },
   )
@@ -50,6 +56,6 @@ function Render() {
 
 <template>
   <!-- This style is combined with `github.min.css` & `github-dark.min.css` -->
-  <link rel="stylesheet" href="/styles/hightlight-js/github.min.css">
+  <link rel="stylesheet" href="/styles/highlight-js/github.min.css">
   <Render />
 </template>
